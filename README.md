9aa4218fa6c727980f4a64e0d2fd9a2ca129c624
ymahendraa <ymahendra721@gmail.com>
2021-05-19 11:34:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# node-basic-authentication-api
+
+NodeJS Basic HTTP Authentication API
+
+For documentation and instructions check out https://jasonwatmore.com/post/2018/09/24/nodejs-basic-authentication-tutorial-with-example-api
---
+++ server.js
@@ -0,0 +1,40 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const basicAuth = require('_helpers/basic-auth');
+const errorHandler = require('_helpers/error-handler');
+
+const https = require('https');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use basic HTTP auth to secure the api
+app.use(basicAuth);
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+// const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+// const server = app.listen(port, function () {
+//     console.log('Server listening on port ' + port);
+// });
+
+// // listen to https
+const httpsServer = https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app);
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
